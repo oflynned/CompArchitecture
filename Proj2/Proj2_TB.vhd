20e90e2aeaa67572ba12712403f66a64132856a4
oflynned <oflynned@tcd.ie>
2016-03-25 10:03:52 +0000

    pushing some updated vhd files for test benches

--- Proj2/Proj2_TB.vhd
+++ Proj2/Proj2_TB.vhd
@@ -1,36 +1,5 @@
---------------------------------------------------------------------------------
--- Company: 
--- Engineer:
---
--- Create Date:   09:20:55 03/25/2016
--- Design Name:   
--- Module Name:   C:/Users/Ed/CS2022/Proj2/Proj2_TB.vhd
--- Project Name:  Proj2
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: Proj2
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
---------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
- 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---USE ieee.numeric_std.ALL;
  
 ENTITY Proj2_TB IS
 END Proj2_TB;
@@ -75,12 +44,7 @@ BEGIN
    -- Stimulus process
    stim_proc: process
    begin		
-      -- hold reset state for 100 ns.
-      wait for 100 ns;	
-
-      wait for Clk_period*10;
-
-      -- insert stimulus here 
+		
 
       wait;
    end process;
--- Proj2/RegFile_TB.vhd
+++ Proj2/RegFile_TB.vhd
@@ -1,36 +1,5 @@
---------------------------------------------------------------------------------
--- Company: 
--- Engineer:
---
--- Create Date:   09:26:28 03/25/2016
--- Design Name:   
--- Module Name:   C:/Users/Ed/CS2022/Proj2/RegFile_TB.vhd
--- Project Name:  Proj2
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: RegFile
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
---------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
- 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---USE ieee.numeric_std.ALL;
  
 ENTITY RegFile_TB IS
 END RegFile_TB;
@@ -95,12 +64,64 @@ BEGIN
    -- Stimulus process
    stim_proc: process
    begin		
-      -- hold reset state for 100 ns.
-      wait for 100 ns;	
-
-      wait for Clk_period*10;
-
-      -- insert stimulus here 
+		load_in <= '1'
+		des_d <= "0000";
+		data <= x"FFFF";
+		wait for 10ns;
+		
+		des_d <= "0001";
+		data <= x"EEEE";
+		wait for 10ns;
+		
+		des_d <= "0010";
+		data <= x"DDDD";
+		wait for 10ns;
+		
+		des_d <= "0011";
+		data <= x"CCCC";
+		wait for 10ns;
+		
+		des_d <= "0100";
+		data <= x"BBBB";
+		wait for 10ns;
+		
+		des_d <= "0101";
+		data <= x"AAAA";
+		wait for 10ns;
+		
+		des_d <= "0110";	
+		data <= x"9999";
+		wait for 10ns;
+			
+		des_d <= "0111";
+		data <= x"8888";
+		wait for 10ns;
+		
+		des_d <= "1000";
+		data <= x"7777";
+		wait for 10ns;
+		
+		load_in <= '0';
+		add_a <= "0000";
+		add_b <= "0111";
+		wait for 5ns;
+		
+		add_a <= "0001";
+		add_b <= "0110";
+		wait for 5ns;
+		
+		add_a <= "0010";
+		add_b <= "0101";
+		wait for 5ns;
+		
+		add_a <= "0011";
+		add_b <= "0100";
+		wait for 5ns;
+		
+		add_a <= "1000";
+		add_b <= "1111";
+		wait for 5ns;
+		
 
       wait;
    end process;
